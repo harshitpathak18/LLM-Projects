39f9dff8a08dcf6db567684480420167f5179187
Harshit Pathak <90919795+harshitpathak18@users.noreply.github.com>
2024-11-18 18:30:11 +0530

    Update app.py

--- PDF_Chat_Bot/app.py
+++ PDF_Chat_Bot/app.py
@@ -120,7 +120,7 @@ def get_vector_store(chunks):
     Args:
         chunks (list): List of text chunks.
     """
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-004")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
     # Remove the existing FAISS index directory if it exists
     if os.path.exists("faiss_index"):
