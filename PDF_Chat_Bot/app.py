ef17f3f56945f347db4dc81277920dec2cd22951
Harshit Pathak <90919795+harshitpathak18@users.noreply.github.com>
2024-11-18 13:20:50 +0530

    Update app.py

--- PDF_Chat_Bot/app.py
+++ PDF_Chat_Bot/app.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import streamlit as st
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
@@ -12,41 +11,8 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredPDFLoader
 
-
 # Load environment variables from the .env file
 load_dotenv()
-
-
-# Set up logging to both console and a file
-def setup_logging():
-    """
-    Sets up logging to both console and file.
-    """
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    # Create a file handler to write logs to a file
-    file_handler = logging.FileHandler('app_logs.log')
-    file_handler.setLevel(logging.INFO)
-
-    # Create a formatter and set it for the file handler
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
-
-    # Add the file handler to the logger
-    logger.addHandler(file_handler)
-
-    # Optionally, add a stream handler to output logs to the console
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-
-    return logger
-
-# Set up logging when the script starts
-logger = setup_logging()
-
 
 # Check for Google API key
 if "GOOGLE_API_KEY" not in os.environ:
@@ -133,7 +99,7 @@ def get_text_chunks(text):
     Returns:
         list: List of text chunks.
     """
-    splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=100)
     chunks = splitter.split_text(text)
     return chunks
 
@@ -148,7 +114,6 @@ def get_vector_store(chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-    logger.info("Vector store saved locally.")
 
 # Create a conversational QA chain
 def get_conversational_chain():
@@ -159,13 +124,12 @@ def get_conversational_chain():
         Langchain chain: Configured QA chain for handling user questions.
     """
     prompt_template = """
-    Answer the question as detailed as possible from the provided context and try to find out that question. please answer the question from the context only. 
-    don't answer outside the context.
+    Answer the question as detailed as possible from the provided context from single of multiple pdfs"
     Context:\n {context}?\n
     Question: \n{question}\n
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.8)
+    model = ChatGoogleGenerativeAI(model="gemini-1.0-pro", temperature=0.5)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
     return chain
@@ -175,8 +139,7 @@ def clear_chat_history():
     """
     Clears the chat history stored in the session state.
     """
-    st.session_state.messages = [{"role": "assistant", "content": "Upload some PDFs and ask me a question."}]
-    logger.info("Chat history cleared.")
+    st.session_state.messages = [{"role": "assistant", "content": "Ask me any question."}]
 
 # Handle user input and fetch response from vector store
 def user_input(user_question):
@@ -196,33 +159,17 @@ def user_input(user_question):
     chain = get_conversational_chain()
     response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
     
-    logger.info(f"User question: {user_question}")
-    logger.info(f"Response generated: {response['output_text']}")
-    
     return response
 
 def main():
     """
     Main function to drive the application. Handles the user interface, file uploads, and interactions.
     """
-    # Sidebar for uploading PDFs
-    # st.sidebar.title('PDF QueryBot')
-    # with st.sidebar:
-    #     pdf_docs = st.file_uploader("Upload PDF Files then Submit & Process", accept_multiple_files=True)
-    #     if st.button("Submit & Process"):
-    #         with st.spinner("Processing..."):
-    #             raw_text = get_pdf_text(pdf_docs)
-    #             text_chunks = get_text_chunks(raw_text)
-    #             get_vector_store(text_chunks)
-    #             st.success("Processing done. Now you can ask questions.")
-
-    # st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
+    
     # Main content area
     st.title("PDF QueryBot")
     st.write("Effortless Conversations with Your Documents!")
-    
-    pdf_docs = st.file_uploader("Upload PDF Files then Submit & Process", accept_multiple_files=True)
+    pdf_docs = st.file_uploader("Upload PDF Files", accept_multiple_files=True)
     if st.button("Submit & Process"):
         with st.spinner("Processing..."):
             raw_text = get_pdf_text(pdf_docs)
@@ -230,14 +177,11 @@ def main():
             get_vector_store(text_chunks)
             st.success("Processing done. Now you can ask questions.")
 
-    st.button('Clear Chat History', on_click=clear_chat_history)
-        
-        
     # Only allow querying if PDFs are uploaded and processed
     if "faiss_index" in os.listdir() and pdf_docs:  
         # Initialize chat history if not present
         if "messages" not in st.session_state.keys():
-            st.session_state.messages = [{"role": "assistant", "content": "Upload some PDFs and ask me a question."}]
+            st.session_state.messages = [{"role": "assistant", "content": "Ask me any question."}]
 
         # Display the chat history
         for message in st.session_state.messages:
@@ -253,7 +197,7 @@ def main():
             # Get the bot's response to the user input
             if st.session_state.messages[-1]["role"] != "assistant":
                 with st.chat_message("assistant"):
-                    with st.spinner("Thinking..."):
+                    with st.spinner("Generating Answer..."):
                         response = user_input(prompt)
                         placeholder = st.empty()
                         full_response = ''
@@ -265,10 +209,11 @@ def main():
                 # Append the assistant's response to the session state
                 message = {"role": "assistant", "content": full_response}
                 st.session_state.messages.append(message)
-                logger.info(f"Assistant response: {full_response}")
+
+            st.button('Clear Chat History', on_click=clear_chat_history)
     else:
         st.write("Please upload some PDFs and process them before asking questions.")
-
+    
 if __name__ == "__main__":
     style_page()
     main()
