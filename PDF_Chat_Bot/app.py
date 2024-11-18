66c42a8240683da861c2387e2d045c672c778395
Harshit Pathak <90919795+harshitpathak18@users.noreply.github.com>
2024-11-18 18:24:32 +0530

    Update app.py

--- PDF_Chat_Bot/app.py
+++ PDF_Chat_Bot/app.py
@@ -18,6 +18,7 @@ load_dotenv()
 # Check for Google API key
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+    os.environ["GOOGLE_API_KEY"] = "AIzaSyCzwsSx2oFmRdpOEd4l-QJOb36J6VSQ1Yk"
 
 # Styling for the Streamlit page
 def style_page():
